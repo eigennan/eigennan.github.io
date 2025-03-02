2be8383566d25894947812ccb4b7064242e2b789
Nan Li <101618451+eigenan@users.noreply.github.com>
2025-03-02 09:32:31 -0600

    projects

--- src/pages/projects/projects.ts
+++ src/pages/projects/projects.ts
@@ -3,7 +3,7 @@ import { getRepositoryDetails } from "../../utils";
 export interface Project {
   name: string;
   demoLink: string;
-  // tags?: string[],
+  tags?: string[],
   description?: string;
   postLink?: string;
   demoLinkRel?: string;
@@ -15,43 +15,51 @@ export const projects: Project[] = [
     name: 'Devaradise.com',
     description: 'A blog that sharing web development resources and tutorials',
     demoLink: 'https://devaradise.com',
+    tags: ["JavaScript", "React"],
   },
   {
     name: 'Sellercraft App',
     description: 'An Ecommerce omnichannel platform in Southeast Asia',
     demoLink: 'https://sellercraft.co',
     demoLinkRel: 'nofollow noopener noreferrer',
+    tags: ["Machine Learning", "Pytorch"],
   },
   {
     name: 'Gaji.id App',
     description: 'Payroll and HR Management Information System',
     demoLink: 'https://sellercraft.co',
     demoLinkRel: 'nofollow noopener noreferrer',
+    tags: ["Data-driven", "Gausssian Process"],
   },
   {
     ...(await getRepositoryDetails('devaradise/paradise-ui')),
     name: 'Paradise UI',
     demoLink: 'https://paradise-ui.com',
     postLink: 'https://devaradise.com/how-i-build-paradise-ui-react-component-library/',
+    tags: ["Front-end", "UI/UX"],
   },
   {
     ...(await getRepositoryDetails('syakirurahman/react-lab')),
     name: 'React Lab',
     demoLink: 'https://devaradise.com/lab/react/',
+    tags: ["Astro", "Web Development"],
   },
   {
     ...(await getRepositoryDetails('syakirurahman/pokemon-catcher')),
     name: 'Pokemon Catcher',
     demoLink: 'https://pokemon-catcher-18636.web.app/',
+    tags: ["Machine Learning", "UI/UX"],
   },
   {
     ...(await getRepositoryDetails('syakirurahman/movie-nominations')),
     name: 'Movie Nominations',
     demoLink: 'https://movie-nominations-c21c3.web.app/',
+    tags: ["Pytorch", "React"],
   },
   {
     ...(await getRepositoryDetails('syakirurahman/organization-tree')),
     name: 'Organization tree',
     demoLink: 'https://organization-tree-2a446.web.app/',
+    tags: ["Machine Learning", "Gaussian Process"],
   }
 ]
