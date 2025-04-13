afa10492a5036a6064620663a27ef74b4b2eaffb
Nan Li <101618451+eigenan@users.noreply.github.com>
2025-04-12 22:46:06 -0500

    new pub

---
+++ src/content/blog_archive/2025-03-30-center-manifold/index.md
@@ -0,0 +1,19 @@
+---
+title: 'Computing Center Manifolds for Infinite-dimensional Dynamical Systems'
+seoTitle: 'Computing Center Manifolds for Infinite-dimensional Dynamical Systems'
+slug: 'center-manifolds-in-infinite-dimensional-dynamical-systems'
+description: 'Center Manifolds in Infinite-dimensional Dynamical Systems'
+pubDate: '2025-03-30'
+updatedDate: '2025-03-30'
+tags: ["Dynamical Systems", "Center Manifold"]
+# coverImage: './astro-banner.png'
+---
+
+## Center Manifolds
+
+What is a center manifold?
+
+## References
+[Local Bifurcations, Center Manifolds, and Normal Forms in Infinite-Dimensional Dynamical Systems by Mariana Haragus, Gérard Iooss][1]
+
+[1]: https://link.springer.com/book/10.1007/978-0-85729-112-7
--- src/pages/publications/publications.ts
+++ src/pages/publications/publications.ts
@@ -21,6 +21,50 @@ export interface Publications {
 
 export const publications: Publications[] = [
   {
+    name: 'Instability of anchored spirals in geometric flows',
+    abstract: 'We investigate existence, stability, and instability of anchored rotating spiral waves in a model for geometric curve evolution. We find existence in a parameter regime limiting on a purely eikonal curve evolution. We study stability and instability both theoretically in this limiting regime and numerically, finding both oscillatory, at first convective instability, and saddle-node bifurcations. Our results in particular shed light onto instability of spiral waves in reaction-diffusion systems caused by an instability of wave trains against transverse modulations.',
+    paperLink: 'https://arxiv.org/abs/2504.07270',
+    authors: [
+        {
+          name: 'Anthony Cortez',
+          url: 'https://cse.umn.edu/math/anthony-cortez'
+        },
+        {
+          name: 'Nan Li',
+          url: 'https://nanli.co'
+        },
+        {
+          name: 'Nathan Mihm',
+          url: ''
+        },
+        {
+          name: 'Alice Xu',
+          url: ''
+        },
+        {
+          name: 'Xiaoxing Yu',
+          url: ''
+        },
+        {
+          name: 'Arnd Scheel',
+          url: 'https://www.math.umn.edu/~scheel/'
+        }
+      ],
+    journal: 'Preprint',
+    year: 2025,
+    image: '/instability.png',
+    pdf: '/Instability of anchored spirals in geometric flows.pdf',
+    bibtex: `@article{cortez2025instabilityanchoredspiralsgeometric,
+      title={Instability of anchored spirals in geometric flows}, 
+      author={Anthony Cortez and Nan Li and Nathan Mihm and Alice Xu and Xiaoxing Yu and Arnd Scheel},
+      year={2025},
+      eprint={2504.07270},
+      archivePrefix={arXiv},
+      primaryClass={nlin.PS},
+      url={https://arxiv.org/abs/2504.07270}, 
+      }`,
+  },
+  {
     name: 'Anchored spirals in the driven curvature flow approximation',
     abstract: 'We study existence, asymptotics, and stability of spiral waves in a driven curvature approximation, supplemented with an anchoring condition on a circle of finite radius. We analyze the motion of curves written as graphs in polar coordinates, finding spiral waves as rigidly rotating shapes. The existence analysis reduces to a planar ODE and asymptotics are given through center manifold expansions. In the limit of a large core, we find rotation frequencies and corrections starting form a problem without curvature corrections. Finally, we demonstrate orbital stability of spiral waves by exploiting a comparison principle inherent to curvature driven flow.',
     paperLink: 'https://doi.org/10.48550/arXiv.2312.07809',
@@ -61,7 +105,8 @@ export const publications: Publications[] = [
             url: 'https://mikehill.math.umn.edu'
         },
         {
-            name: 'Jiayun Meng'
+            name: 'Jiayun Meng',
+            url: ''
         },
         {
             name: 'Nan Li',
