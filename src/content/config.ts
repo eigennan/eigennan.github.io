82188636b80c350fb54c63b46391e160edf61954
Nan Li <7638451+eigennan@users.noreply.github.com>
2025-12-09 13:42:31 -0600

    blog cover image size

--- src/content/config.ts
+++ src/content/config.ts
@@ -12,11 +12,7 @@ const blog = defineCollection({
 		updatedDate: z.coerce.date().optional(),
     tags: z.array(z.string()).optional(),
 		draft: z.boolean().optional(),
-		coverImage: image()
-      .refine((img) => img.width >= 960, {
-        message: 'Cover image must be at least 960 pixels wide!'
-      })
-      .optional()
+		coverImage: image().optional()
 	}),
 });
 
