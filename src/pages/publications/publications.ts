e858d131e67bbeb9d4f778c03bfe611388f55e68
Nan Li <7638451+eigennan@users.noreply.github.com>
2025-12-24 09:51:56 -0600

    pub

--- src/pages/publications/publications.ts
+++ src/pages/publications/publications.ts
@@ -21,7 +21,7 @@ export interface Publications {
 
 export const publications: Publications[] = [
   {
-    name: 'Instability of anchored spirals in geometric flows',
+    name: 'Instability of Anchored Spirals in Geometric Flows',
     abstract: 'We investigate existence, stability, and instability of anchored rotating spiral waves in a model for geometric curve evolution. We find existence in a parameter regime limiting on a purely eikonal curve evolution. We study stability and instability both theoretically in this limiting regime and numerically, finding both oscillatory, at first convective instability, and saddle-node bifurcations. Our results in particular shed light onto instability of spiral waves in reaction-diffusion systems caused by an instability of wave trains against transverse modulations.',
     paperLink: 'https://arxiv.org/abs/2504.07270',
     authors: [
@@ -67,7 +67,7 @@ export const publications: Publications[] = [
     tags: ["Preprint", "Geometric Flows", "Spiral Waves"],
   },
   {
-    name: 'Anchored spirals in the driven curvature flow approximation',
+    name: 'Anchored Spirals in the Driven Curvature Flow Approximation',
     abstract: 'We study existence, asymptotics, and stability of spiral waves in a driven curvature approximation, supplemented with an anchoring condition on a circle of finite radius. We analyze the motion of curves written as graphs in polar coordinates, finding spiral waves as rigidly rotating shapes. The existence analysis reduces to a planar ODE and asymptotics are given through center manifold expansions. In the limit of a large core, we find rotation frequencies and corrections starting form a problem without curvature corrections. Finally, we demonstrate orbital stability of spiral waves by exploiting a comparison principle inherent to curvature driven flow.',
     paperLink: 'https://doi.org/10.48550/arXiv.2312.07809',
     authors: [
@@ -97,7 +97,7 @@ export const publications: Publications[] = [
     tags: ["Journal Article", "Geometric Flows", "Spiral Waves"],
   },
   {
-    name: 'Counting compatible indexing systems for $C_{p^n}$',
+    name: 'Counting Compatible Indexing Systems for $C_{p^n}$',
     abstract: 'We count the number of compatible pairs of indexing systems for the cyclic group $C_{p^n}$. Building on work of Balchin--Barnes--Roitzheim, we show that this sequence of natural numbers is another family of Fuss--Catalan numbers. We count this two different ways: showing how the conditions of compatibility give natural recursive formulas for the number of admissible sets and using an enumeration of ways to extend indexing systems by conceptually simpler pieces.',
     paperLink: 'https://doi.org/10.2140/om.2024.1.37',
     paperLinkRel: 'nofollow noopener noreferrer',
