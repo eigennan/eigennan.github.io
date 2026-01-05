bfd8efd14120c7f7d40ba243f6f76a881a8c79ad
Nan Li <7638451+eigennan@users.noreply.github.com>
2026-01-05 10:15:55 -0600

    pub cards

--- .astro/content-assets.mjs
+++ .astro/content-assets.mjs
@@ -1,6 +1,6 @@
 
-import __ASTRO_IMAGE_IMPORT_Zw2iaJ from "./blog-placeholder-1.jpg?astroContentImageFlag=&importer=src%2Fcontent%2Fblog%2F2025-02-24-blog-template%2Findex.md";
 import __ASTRO_IMAGE_IMPORT_Z2vnVNq from "./manifold.png?astroContentImageFlag=&importer=src%2Fcontent%2Fblog%2F2025-11-30-center-manifold-example%2Findex.md";
 import __ASTRO_IMAGE_IMPORT_Z17jBK from "./secant_continuation.png?astroContentImageFlag=&importer=src%2Fcontent%2Fblog%2F2025-12-01-secant-continuation%2Findex.md";
-export default new Map([["./blog-placeholder-1.jpg?astroContentImageFlag=&importer=src%2Fcontent%2Fblog%2F2025-02-24-blog-template%2Findex.md", __ASTRO_IMAGE_IMPORT_Zw2iaJ], ["./manifold.png?astroContentImageFlag=&importer=src%2Fcontent%2Fblog%2F2025-11-30-center-manifold-example%2Findex.md", __ASTRO_IMAGE_IMPORT_Z2vnVNq], ["./secant_continuation.png?astroContentImageFlag=&importer=src%2Fcontent%2Fblog%2F2025-12-01-secant-continuation%2Findex.md", __ASTRO_IMAGE_IMPORT_Z17jBK]]);
+import __ASTRO_IMAGE_IMPORT_Zw2iaJ from "./blog-placeholder-1.jpg?astroContentImageFlag=&importer=src%2Fcontent%2Fblog%2F2025-02-24-blog-template%2Findex.md";
+export default new Map([["./manifold.png?astroContentImageFlag=&importer=src%2Fcontent%2Fblog%2F2025-11-30-center-manifold-example%2Findex.md", __ASTRO_IMAGE_IMPORT_Z2vnVNq], ["./secant_continuation.png?astroContentImageFlag=&importer=src%2Fcontent%2Fblog%2F2025-12-01-secant-continuation%2Findex.md", __ASTRO_IMAGE_IMPORT_Z17jBK], ["./blog-placeholder-1.jpg?astroContentImageFlag=&importer=src%2Fcontent%2Fblog%2F2025-02-24-blog-template%2Findex.md", __ASTRO_IMAGE_IMPORT_Zw2iaJ]]);
 		
